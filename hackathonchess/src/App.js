96afdb34bb9eff60469920c5a42161ad16dcd100
Luca <Lucaprenzler1@gmail.com>
2021-12-06 14:25:30 +0200

    🎨 Initialized the firebase app

--- hackathonchess/src/App.js
+++ hackathonchess/src/App.js
@@ -1,7 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { initializeApp } from "firebase/app";
+
+import { doc, setDoc, Timestamp, getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMkg01S-9Al-U99ocqbQSGhxCygl8ZBW4",
+  authDomain: "realtime-chess-abde5.firebaseapp.com",
+  projectId: "realtime-chess-abde5",
+  storageBucket: "realtime-chess-abde5.appspot.com",
+  messagingSenderId: "1090941740351",
+  appId: "1:1090941740351:web:1680feb601abb474169f8a",
+  measurementId: "G-P48DT1P0V3"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore();
+
+
 function App() {
+
+  // const docData = {
+  //   stringExample: "Hello world!",
+  //   booleanExample: true,
+  //   numberExample: 3.14159265,
+  //   dateExample: Timestamp.fromDate(new Date("December 10, 1815")),
+  //   arrayExample: [5, true, "hello"],
+  //   nullExample: null,
+  //   objectExample: {
+  //       a: 5,
+  //       b: {
+  //           nested: "foo"
+  //       }
+  //     }
+  // };
+  // setDoc(doc(db, "data", "one"), docData);
+
   return (
     <div className="App">
       <header className="App-header">
