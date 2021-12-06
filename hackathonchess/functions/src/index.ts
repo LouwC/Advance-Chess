f682f31c0f2c509f0425aedb1b882a17d93ed9e6
Louw707 <u18000402@tuks.co.za>
2021-12-06 14:52:11 +0200

    🚧 (Functions): Started creating cloud functions

--- hackathonchess/functions/src/index.ts
+++ hackathonchess/functions/src/index.ts
@@ -1,9 +1,28 @@
 import * as functions from "firebase-functions";
+import { doc, setDoc, getFirestore } from "firebase/firestore";
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
-});
+
+export const createUser = functions.firestore
+    .document('events_test/{event_id}')
+    .onCreate(async (snap) => {
+        // Get an object representing the document
+        // e.g. {'name': 'Marie', 'age': 66}
+        const newValue = snap.data();
+
+        const db = getFirestore();
+        // access a particular field as you would any JS property
+
+
+        // Add a new document in collection "cities"
+        await setDoc(doc(db, "cities", "LA"), {
+            name: "Los Angeles",
+            state: "CA",
+            country: "USA",
+            ...<any>newValue
+        });
+
+        // perform desired operations ...
+    });
